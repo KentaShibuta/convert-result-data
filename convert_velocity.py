632f080d31f147964c5980c7bec460d3e0c5535b
澁田謙太 <kenta@kentanoMacBook-Air-2.local>
2020-01-03 00:10:35 +0900

    update

--- convert_velocity.py
+++ convert_velocity.py
@@ -3,8 +3,9 @@ import math
 import cmath
 
 def conv_v(cul_point, v_data):
-    vx = v_data[0][0]*0.25*(1-cul_point[0])*(1-cul_point[1]) + v_data[1][0]*0.25*(1+cul_point[0])*(1-cul_point[1]) + v_data[2][0]*0.25*(1+cul_point[0])*(1+cul_point[1]) + v_data[3][0]*0.25*(1-cul_point[0])*(1+cul_point[1])
-    vy = v_data[0][1]*0.25*(1-cul_point[0])*(1-cul_point[1]) + v_data[1][1]*0.25*(1+cul_point[0])*(1-cul_point[1]) + v_data[2][1]*0.25*(1+cul_point[0])*(1+cul_point[1]) + v_data[3][1]*0.25*(1-cul_point[0])*(1+cul_point[1])
+    for i in range(4500):
+        vx = v_data[0][0]*0.25*(1-cul_point[0])*(1-cul_point[1]) + v_data[1][0]*0.25*(1+cul_point[0])*(1-cul_point[1]) + v_data[2][0]*0.25*(1+cul_point[0])*(1+cul_point[1]) + v_data[3][0]*0.25*(1-cul_point[0])*(1+cul_point[1])
+        vy = v_data[0][1]*0.25*(1-cul_point[0])*(1-cul_point[1]) + v_data[1][1]*0.25*(1+cul_point[0])*(1-cul_point[1]) + v_data[2][1]*0.25*(1+cul_point[0])*(1+cul_point[1]) + v_data[3][1]*0.25*(1-cul_point[0])*(1+cul_point[1])
 
     v = np.array([vx,vy])
 
--- input_v.py
+++ input_v.py
@@ -1,43 +1,44 @@
 import numpy as np
 
-def inp():
-    print('Input node file name >>>>')
-    f_name_node = input().rstrip() # 可視化するファイル名
-
+def inp_v():
     print('Input nbool file name >>>>')
     f_name_nbool = input().rstrip() # 可視化するファイル名
+
+    print('Input velocity file name >>>>')
+    f_name_velocity = input().rstrip() # 可視化するファイル名
 
     ############################################
     #elem_node[nelem][4] # 要素番号と節点番号の対応
     #node_data[nnode][2]　
     # shellできざんできたファイルから読み取る．
     # shellできざまなくてもpythonできざむことができるよね．
-    elem_node = np.loadtxt(f_name_nbool,
-                        delimiter=",",
-                        skiprows=0,
-                        usecols=(3,4,5,6)
-                        )
-    # 要素番号と節点番号の対応
 
-    node_data = np.loadtxt(f_name_node,
+    elem_node = np.loadtxt(f_name_nbool,
+                    delimiter=",",
+                    skiprows=0,
+                    usecols=(3,4,5,6)
+                    )
+    # 要素番号と節点番号の対応
+    node_velocity = np.loadtxt(f_name_velocity,
                         delimiter=",",
                         skiprows=0,
                         usecols=(1,2)
                         )
+
     
     nelem = int(elem_node.shape[0])
 
-    elem_node_data = np.zeros((nelem,4,2))
+    elem_node_velocity = np.zeros((nelem,4,2))
 
     #print(nelem)
 
     for i in range(int(nelem)):
-        elem_node_data[i][0] = node_data[int(elem_node[i][0])]
-        elem_node_data[i][1] = node_data[int(elem_node[i][1])]
-        elem_node_data[i][2] = node_data[int(elem_node[i][2])]
-        elem_node_data[i][3] = node_data[int(elem_node[i][3])]
+        elem_node_velocity[i][0] = node_velocity[int(elem_node[i][0])]
+        elem_node_velocity[i][1] = node_velocity[int(elem_node[i][1])]
+        elem_node_velocity[i][2] = node_velocity[int(elem_node[i][2])]
+        elem_node_velocity[i][3] = node_velocity[int(elem_node[i][3])]
 
     #np.save('./output.txt', elem_node_data)
     #np.savetxt('./np_savetxt.csv', elem_node_data, delimiter=',')
-    return elem_node_data
+    return elem_node_velocity
 
