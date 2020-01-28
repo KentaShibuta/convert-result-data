fc21d59cbea389534e1adab625b208bb8bd3ad0c
澁田謙太 <kenta@kentanoMacBook-Air-2.local>
2020-01-28 19:18:40 +0900

    update

--- create_new_mesh.py
+++ create_new_mesh.py
@@ -1,29 +1,29 @@
 import numpy as np
 
 def new_mesh():
-    L_x = 0.01
-    L_y = 0.01
-    x_elem = 10
-    y_elem = 10
+    L_x = 1.0
+    L_y = 1.0
+    x_elem = 30
+    y_elem = 30
     dx = L_x / x_elem
     dy = L_y / y_elem
     x_node = x_elem + 1
     y_node = y_elem + 1
-    x = -0.005
-    y = -0.005
+    x = -0.5
+    y = -0.5
     node_val = np.zeros((x_node*y_node,2))
     num = 0
     for j in range(y_node):
         for i in range(x_node):
             if i == 0:
-                x=-0.005
+                x=-0.5
             else:
-                x = x + dx 
+                x = x + dx
             node_val[num][0]=x
             node_val[num][1]=y
             num = num + 1
         y = y + dy
-        x = -0.005
+        x = -0.5
     node_val_np = np.array(node_val)
     print(node_val_np)
 
